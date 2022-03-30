23cb60e2dc5570cfb82ec52876c7da75cb9c40d5
Maradonner <102668322+Maradonner@users.noreply.github.com>
2022-03-30 17:03:04 +0500

    Add files via upload

---
+++ task11/task11/ClassQueue.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ClassLibrary1;
+using System.Linq;
+
+namespace task11
+{
+    class ClassQueue:ICloneable
+    {
+        Queue<Person> people = null;
+    
+        public ClassQueue()
+        {
+            people = new Queue<Person>();
+        }
+
+        public Queue<Person> AddElement(Person elem)
+        {
+            people.Enqueue(elem);
+            return people;
+        }
+
+        public Queue<Person> DeleteElement()
+        {
+            if(people.Count!=0)
+                people.Dequeue();
+            else
+                Console.WriteLine("Нельзя удалить элемент. Очередь пустая!");
+            return people;
+        }
+
+        public void Show()
+        {
+            foreach (var person in people)
+            {
+                Console.WriteLine(person);
+            }
+        }
+
+        public void PersonCount()
+        {
+            int i = 0;
+            foreach (var person in people)
+            {
+                i++;
+            }
+            Console.WriteLine(i);
+        }
+
+        public void EngineerPrint()
+        {
+            foreach (Person p in people)
+            {
+                if (p is Engineer)
+                {
+                    Console.WriteLine(p);
+                }
+            }
+        }
+
+        public void WorkerPrint()
+        {
+            foreach (Person p in people)
+            {
+                if (p is Worker)
+                {
+                    Console.WriteLine(p);
+                }
+            }
+        }
+
+        public object Clone()
+        {
+            Queue<Person> clonable = new Queue<Person>();
+            foreach (var elem in people)
+            {
+                clonable.Enqueue((Person)elem.Clone());
+            }
+            return clonable;
+        }
+
+        public object ShallowCopy()
+        {
+            return this.MemberwiseClone();
+        }
+
+        public Queue<Person> Sort()
+        {
+            var list = people.ToList();
+            list.Sort();
+            people = new Queue<Person>(list);
+            return people;
+        }
+
+        public void Search(Person elem)
+        {
+            bool find = false;
+            foreach (var person in people)
+            {
+               if ((person.ToString()).Contains(elem.ToString()))
+                {
+                    find = true;
+                    Console.WriteLine(elem);
+                }
+ 
+            }
+            if (!find)
+                Console.WriteLine("Не нашлось!!!");
+        }
+
+
+    }
+}
+
+
+
+
+    
---
+++ task11/task11/ClassSortedDictionary.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ClassLibrary1;
+using System.Linq;
+
+namespace task11
+{
+    class ClassSortedDictionary:ICloneable
+    {
+        SortedDictionary<Person, Worker> dictionary = null;
+
+        public ClassSortedDictionary()
+        {
+            dictionary = new SortedDictionary<Person, Worker>();
+        }
+
+        public SortedDictionary<Person, Worker> AddElement(Person key, Worker value)
+        {
+            dictionary.Add(key, value);
+            return dictionary;
+        }
+
+        public SortedDictionary<Person, Worker> DeleteElement(Person key)
+        {
+            dictionary.Remove(key);
+            return dictionary;
+        }
+
+        public void Show()
+        {
+            foreach(var element in dictionary )
+                Console.WriteLine(element);
+        }
+
+        public object Clone()
+        {
+            SortedDictionary<Person, Worker> clonable = new SortedDictionary<Person, Worker>();
+
+            foreach (var element in dictionary)
+            {
+                Person key = (Person)element.Key.Clone();
+                Worker value = (Worker)element.Value.Clone();
+                clonable.Add(key, value);
+            }
+
+            return clonable;
+        }
+
+        public void ShowWorkers()
+        {
+            foreach (var element in dictionary)
+            {
+                Console.WriteLine(element.Value);
+            }  
+        }
+
+        public void ShowPersons()
+        {
+            foreach (var element in dictionary)
+            {
+                Console.WriteLine(element.Key);
+            }
+        }
+
+        public void Length()
+        {
+            int count = 0;
+            foreach (var element in dictionary)
+            {
+                count++;
+            }
+            Console.WriteLine(count);
+        }
+
+        public void Sort()
+        {
+            foreach (var element in dictionary.OrderBy(key => key.Value.Name))
+                Console.WriteLine(element);
+        }
+
+
+    }
+}
